980c5e96b858dafb924a53ef46c8ee3add647498
--replace-all <akhbharti@gmail.com>
2019-05-28 23:07:49 +0530

    dice game snippet

---
+++ src/RollDice.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import Die from "./die";
+import "./RollDice.css";
+class RollDice extends Component {
+  static defaultProps = {
+    sides: ["one", "two", "three", "four", "five", "six"]
+  };
+  constructor(props) {
+    super(props);
+    this.state = { die1: "one", die2: "one", isRolling: false };
+  }
+
+  rollDice = () => {
+    //pick 2 new rolls
+    const newDie1 = this.props.sides[
+      Math.floor(Math.random() * this.props.sides.length)
+    ];
+    const newDie2 = this.props.sides[
+      Math.floor(Math.random() * this.props.sides.length)
+    ];
+
+    this.setState({ die1: newDie1, die2: newDie2, isRolling: true });
+    //wait one sec and reset the dice text
+    setTimeout(() => {
+      this.setState({ isRolling: false });
+    }, 1000);
+  };
+  render() {
+    return (
+      <div className="RollDice">
+        <div className="RollDice-container">
+          <Die face={this.state.die1} rolling={this.state.isRolling} />
+          <Die face={this.state.die2} rolling={this.state.isRolling} />
+        </div>
+
+        <button onClick={this.rollDice} disabled={this.state.isRolling}>
+          {this.state.isRolling ? "Rolling..." : "Roll Dice!"}
+        </button>
+      </div>
+    );
+  }
+}
+
+export default RollDice;
